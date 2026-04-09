5b3afb06eaac125eaafee45299f4bc12b91a7483
Pedro Henrique Rebechi <pedrohenriquerebechi@gmail.com>
2026-04-09 20:31:14 -0300

    Removendo variavel de porta na conexao (nao necessaria)

--- src/php/conexao.php
+++ src/php/conexao.php
@@ -3,9 +3,8 @@ $host = "localhost";
 $usuario = "root";
 $senha_db = "";
 $nome_banco = "joinup";
-$port = "3306";
 
-$conn = new mysqli($host, $usuario, $senha_db, $nome_banco, $port);
+$conn = new mysqli($host, $usuario, $senha_db, $nome_banco);
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
